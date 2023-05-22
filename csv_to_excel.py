b891ca7a273a83099824a4eb44132475bdad4fe9
@bellamariz <bellamariz@aluno.puc-rio.br>
2023-05-21 22:58:03 -0300

    Convert csv to excel

---
+++ csv_to_excel.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+csvFiles = ['data', 'media', 'place', 'pool', 'tweets', 'users']
+
+for file in csvFiles:
+  path = "twitter-csv/" + file + ".csv"
+  data = pd.read_csv(path,delimiter=',',nrows=10000)
+
+  # data.columns = data.columns.str.replace(r'.', '_')
+  # data.rename(columns={'_id': 'item_id'}, inplace=True)
+
+  newPath = "twitter-excel/" + file + ".xlsx" 
+  resultExcelFile = pd.ExcelWriter(newPath)
+
+  data.to_excel(resultExcelFile, index=False)
+  resultExcelFile.save()
+
