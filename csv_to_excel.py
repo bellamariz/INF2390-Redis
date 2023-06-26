e37b7384e7416881eca56134466a6971662b9e0c
@bellamariz <bellamariz@aluno.puc-rio.br>
2023-06-26 16:50:40 -0300

    Fix csv to excel conversion

--- csv_to_excel.py
+++ csv_to_excel.py
@@ -4,14 +4,13 @@ csvFiles = ['data', 'media', 'place', 'pool', 'tweets', 'users']
 
 for file in csvFiles:
   path = "twitter-csv/" + file + ".csv"
-  data = pd.read_csv(path,delimiter=',',nrows=10000)
+  data = pd.read_csv(path,delimiter=',',nrows=35000)
 
   # data.columns = data.columns.str.replace(r'.', '_')
   # data.rename(columns={'_id': 'item_id'}, inplace=True)
 
   newPath = "twitter-excel/" + file + ".xlsx" 
-  resultExcelFile = pd.ExcelWriter(newPath)
+  # resultExcelFile = pd.ExcelWriter(newPath)
 
-  data.to_excel(resultExcelFile, index=False)
-  resultExcelFile.save()
+  data.to_excel(newPath, index=False)
 
